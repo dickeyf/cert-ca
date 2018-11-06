d63461e8eaa9d0ba91c6f848c3a614ed3b234126
Francois Dickey <francois.dickey@solacesystems.com>
2018-11-06 16:45:32 -0500

    Fixed signClientCert.sh

--- requests/signClientCert.sh
+++ requests/signClientCert.sh
@@ -4,12 +4,12 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-mkdir ${1}-req
+mkdir -p ${1}-req/certs
 cd ${1}-req
 tar zxf ../$1.tgz
 
-./createCnf.sh client $1
-openssl ca -config ../../intermediate/openssl.cnf -extensions usr_cert -days 3650 -md sha256 -in csr/$1.csr -out certs/$1.crt
+../createCnf.sh client $1
+openssl ca -config csr/$1.cnf -extensions usr_cert -days 3650 -md sha256 -in csr/$1.csr -out certs/$1.crt
 openssl x509 -noout -text -in certs/$1.crt
 cat certs/$1.crt ../../intermediate/certs/ca.crt > certs/${1}-chain.pem
 tar zcf ../${1}-signed.tgz certs/$1*
