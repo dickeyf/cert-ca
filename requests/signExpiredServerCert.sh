69101eda94cb4482975303ab694235067a631361
Francois Dickey <francois.dickey@solace.com>
2024-05-30 14:15:33 -0400

    Adding scripts that signs expired certificates and not yet valid certificates for testing purposes.

---
+++ requests/signExpiredServerCert.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+if [ $# -ne 1 ]; then
+  echo "Usage: ./signClientCert.sh <certName>"
+  exit 1
+fi
+
+mkdir -p ${1}-req/certs
+cd ${1}-req
+tar zxf ../$1.tgz
+
+../createCnf.sh server $1
+openssl ca -config csr/$1.cnf -extensions server_cert -startdate 240101010101Z -enddate 240201010101Z -md sha256 -in csr/$1.csr -out certs/$1.crt
+openssl x509 -noout -text -in certs/$1.crt
+cat certs/$1.crt ../../intermediate/certs/ca.crt > certs/${1}-chain.pem
+tar zcf ../${1}-signed.tgz certs/$1*
+cd ..
